26f1352ebdcc19c06cd48abea7ceada1463fd63a
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2022-03-03 15:09:29 +0100

    Add some styling

--- 4._Back_in_the_SSR/app.js
+++ 4._Back_in_the_SSR/app.js
@@ -10,11 +10,19 @@ const footer = fs.readFileSync("./public/components/footer.html").toString();
 const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
 const loops = fs.readFileSync("./public/pages/types/loops.html").toString(); 
 const nodemon = fs.readFileSync("./public/pages/types/nodemon.html").toString(); 
+const ssr = fs.readFileSync("./public/pages/types/SSR.html").toString(); 
+const introduction = fs.readFileSync("./public/pages/types/introduction.html").toString();
+const es = fs.readFileSync("./public/pages/types/express.html").toString();
+const callBack = fs.readFileSync("./public/pages/types/callbackFunctions.html").toString();
 
 //Making these objects will it make it usefull, to send a request to the client. And get a respond t 
 const frontpagePage = nav.replace("%%DOCUMENT_TITLE%%", "Welcome to all type of js. learning goals") + frontpage + footer;
 const loopPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the intro to loops") + loops + footer;
-const nodemonPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the nodemon intro") + nodemon + footer;  
+const nodemonPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the nodemon intro") + nodemon + footer;
+const ssrPage = nav.replace("%%DOCUMENT_TITLE%%", "you are in the ssr intro") + ssr + footer;
+const introductionPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the introduction file") + introduction + footer; 
+const expressPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the express intro") + es + footer; 
+const callbackFunctionPage = nav.replace("%%DOCUMENT_TITLE%%", "you are in the callback intro") + callBack + footer; 
 
 
 //End points for the html sides.
@@ -33,6 +41,21 @@ app.get("/nodemon", (req, res) => {
     res.send(nodemonPage)
 });
 
+app.get("/ssr", (req, res) => {
+    res.send(ssrPage)
+});
 
+
+app.get("/introduction", (req, res) => {
+    res.send(introductionPage)
+});
+
+app.get("/express", (req, res) => {
+    res.send(expressPage)
+});
+
+app.get("/callback", (req, res) => {
+    res.send(callbackFunctionPage)
+});
 //Creating a port 3000 to run the project on 
 app.listen(3000, () => console.log("Server is running on", 3000));
